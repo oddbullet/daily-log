26d6880de43286e53e9ee5ce43f3c47e10a29b93
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-06-02 14:57:59 -0400

    create entryView template

---
+++ src/components/entryView.tsx
@@ -0,0 +1,17 @@
+import "./entryView.css";
+
+export default function EntryView() {
+  return (
+    <>
+      <div className="container">
+        <div className="card-date">
+          <p>June 2, 11:54 AM</p>
+        </div>
+        <div className="card-content">
+          <p>- Applied to Disney</p>
+          <p>- Complete Two Sum II Input Array</p>
+        </div>
+      </div>
+    </>
+  );
+}
