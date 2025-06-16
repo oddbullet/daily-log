59602b2c245b53558c6bc734580c4168c4f73855
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-06-15 22:52:02 -0400

    disable btn when not on today

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Button } from "antd";
 import "./header.css";
+import { useState } from "react";
 
 interface HeaderProp {
   setIsNewEntry: (state: boolean) => void;
@@ -7,21 +8,41 @@ interface HeaderProp {
 }
 
 export default function Header({ setIsNewEntry, setPage }: HeaderProp) {
+  const [entBtnState, setBtnState] = useState(false);
+
   return (
     <div className="header">
       <p className="title">Daily Logger</p>
-      <a className="header-btn" onClick={() => setPage(0)}>
+      <a
+        className="header-btn"
+        onClick={() => {
+          setPage(0);
+          setBtnState(false);
+        }}
+      >
         Today
       </a>
-      <a className="header-btn" onClick={() => setPage(1)}>
+      <a
+        className="header-btn"
+        onClick={() => {
+          setPage(1);
+          setBtnState(true);
+        }}
+      >
         Logs
       </a>
-      <a className="header-btn" onClick={() => setPage(2)}>
+      <a
+        className="header-btn"
+        onClick={() => {
+          setPage(2);
+          setBtnState(true);
+        }}
+      >
         Setting
       </a>
       <Button
         type="primary"
-        disabled={false}
+        disabled={entBtnState}
         onClick={() => setIsNewEntry(true)}
       >
         New Entry
